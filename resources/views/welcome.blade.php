03f549d2b779edae36dbf7f56ea8647eaf1b239a
hcweb <871328529@qq.com>
2019-09-08 11:39:33 +0800

    有没有遗漏文件

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,7 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
+            {{Auth::guest()}}
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
